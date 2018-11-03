f63302215b4d3cd4f836062400894c50bfbc113e
aferrero2707 <aferrero1975@gmail.com>
2018-11-03 15:10:09 +0100

    Update exec.c

--- appimage-exec-wrapper2/exec.c
+++ appimage-exec-wrapper2/exec.c
@@ -70,7 +70,7 @@ static int is_external_process(const char *filename) {
     if (!appdirs)
         return 0;
     DEBUG("APPDIRS = %s\n", appdirs);
-    /*printf("is_external_process: filename = %s\n", filename);*/
+    printf("is_external_process: filename = %s\n", filename);
     /*printf("is_external_process: APPDIRS = %s\n", appdirs);*/
     int pos = 0;
     int len = strlen(appdirs);
@@ -101,7 +101,7 @@ static int is_external_process(const char *filename) {
       pos += 1;
     }
 
-    /*printf("is_external_process: external process found\n");*/
+    printf("is_external_process: external process found\n");
     return 1;
 }
 
